a015fe5d986115b5ee4a9e39a5c69e2262b91ca7
jedbrooke <jasperredrock@gmail.com>
2017-10-07 18:35:38 -0700

    a bunch
    redid images and misc small fixes

--- imageLoading.js
+++ imageLoading.js
@@ -1,13 +1,43 @@
 //scripty mcscriptface
-function loadImage(){
-        var i = Math.floor((Math.random()*237)+1);
-        if(i == 190){
-            i++;
-        }
-        var filePath = "images/stock-images/stock-image_" + i + ".png";
-        var x = document.createElement("IMG");
-        x.setAttribute("src", filePath);
-        x.setAttribute("alt", "Stock Image");
-        x.setAttribute("class", "subpage_image")
-        document.getElementById('stock-image').appendChild(x);
-    }
+function loadImage(type){
+    //0 = any
+    //1 = cnyp (52)
+    //2 = clipart (284)
+    //3 = other (23)
+    switch(type){
+        case 0:
+            var i = Math.floor((Math.random()*3)+1);
+            loadImage(i);
+            break;
+        case 1:
+            var i = Math.floor((Math.random()*52));
+            var filePath = "images/cnyp/image_" + i + ".jpeg";
+            var x = document.createElement("IMG");
+            x.setAttribute("src", filePath);
+            x.setAttribute("alt", "Stock Image");
+            x.setAttribute("class", "subpage_image")
+            document.getElementById('stock-image').appendChild(x);
+            break;
+
+        case 2:
+            var i = Math.floor((Math.random()*284));
+            var filePath = "images/clipart/image_" + i + ".jpeg";
+            var x = document.createElement("IMG");
+            x.setAttribute("src", filePath);
+            x.setAttribute("alt", "Stock Image");
+            x.setAttribute("class", "subpage_image")
+            document.getElementById('stock-image').appendChild(x);
+            break;
+
+        case 3:
+            var i = Math.floor((Math.random()*52));
+            var filePath = "images/other/image_" + i + ".png";
+            var x = document.createElement("IMG");
+            x.setAttribute("src", filePath);
+            x.setAttribute("alt", "Stock Image");
+            x.setAttribute("class", "subpage_image")
+            document.getElementById('stock-image').appendChild(x);
+            break;
+
+    }   
+}
